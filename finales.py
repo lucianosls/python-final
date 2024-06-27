008449cce0260f9a08ea328eb9c120ea736d3cfe
lucianosls <luchosalas111@gmail.com>
2024-06-27 19:33:31 -0300

    Primer commit

---
+++ finales.py
@@ -0,0 +1,31 @@
+"""
+¿Qué es pip?
+
+pip es el sistema de gestión de paquetes para el lenguaje de programación Python, que permite a los usuarios instalar y administrar
+librerías y dependencias adicionales que no están distribuidas como parte de la librería estándar de Python. Esta herramienta es
+esencial para la gestión eficiente de los paquetes necesarios para desarrollar aplicaciones y scripts en Python.
+
+Características principales de pip:
+- Instalación de paquetes: Permite buscar e instalar paquetes publicados en el Python Package Index (PyPI) y otros índices.
+- Actualización de paquetes: Permite actualizar paquetes instalados a versiones más recientes.
+- Gestión de requisitos: Puede instalar paquetes desde un archivo de requisitos, lo cual es útil para asegurar la consistencia
+entre entornos de desarrollo.
+- Desinstalación de paquetes: Permite la eliminación limpia de paquetes instalados.
+
+¿Por qué actualizamos pip?
+1. Seguridad: Las nuevas versiones pueden corregir vulnerabilidades de seguridad en versiones anteriores.
+2. Compatibilidad: Las actualizaciones garantizan compatibilidad con otros paquetes y con las versiones más recientes de Python.
+3. Mejoras de rendimiento: Cada nueva versión de pip suele incluir mejoras en la velocidad y eficiencia de la instalación de paquetes.
+4. Nuevas características: Las actualizaciones incluyen nuevas funcionalidades que facilitan la gestión de paquetes y mejoran la
+experiencia del usuario.
+5. Corrección de errores: Como cualquier otro software, pip puede tener errores que se corrigen en versiones posteriores.
+
+Cómo actualizar pip:
+Actualizar pip es un proceso sencillo y se recomienda hacerlo regularmente para aprovechar las mejoras mencionadas. Se puede
+actualizar pip utilizando el propio pip con el siguiente comando en la terminal:
+
+python -m pip install --upgrade pip
+
+Este comando le dice a Python que ejecute pip como un módulo y actualice pip a la última versión disponible.
+"""
+
